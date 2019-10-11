0e59212d846b8aeb37264fb79f9392dd9a855bfb
lolisound <lolisound@gmail.com>
2019-10-11 13:59:01 +0800

    alias browser-sync

--- Debian/desktop/browser-sync.sh
+++ Debian/desktop/browser-sync.sh
@@ -6,3 +6,6 @@ npm config set registry https://registry.npm.taobao.org
 npm install -g browser-sync
 
 # browser-sync start --server --files "**/*.css, **/*.html"
+# echo -e alias browser-sync=\'browser-sync start --server --files \"**/*.css, **/*.html\"\' >> /etc/bash.bashrc
+echo -e alias browser-sync=\'browser-sync start --server --files \"**/*.css, **/*.html\"\' >> /etc/zsh/zshrc
+
