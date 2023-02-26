4c190d66dfff0665c04f5f37a145a86416ed1db5
MisakaAI <i@zi-o.com>
2023-02-27 06:51:36 +0800

    add php

---
+++ Rocky/php.sh
@@ -0,0 +1,18 @@
+# Nginx
+
+dnf install -y \
+    php-fpm \
+    php-common \
+    php-curl \
+    php-xml \
+    php-gd \
+    php-json \
+    php-mbstring \
+    php-zip \
+    php-pgsql \
+    php-bz2 \
+    php-intl \
+    php-smbclient \
+    php-gmp \
+    php-apcu \
+    php-imagick
