0f5ca917102d98acba160485b279a913b4258df0
MisakaAI <i@zi-o.com>
2023-11-12 19:33:31 +0800

    修改 safe.directory '*'

--- git_config.sh
+++ git_config.sh
@@ -5,7 +5,7 @@ git config --global user.email "i@zi-o.com"
 git config --global color.ui true
 git config --global push.default simple
 git config --global core.autocrlf input
-git config --global safe.directory *
+git config --global safe.directory '*'
 # git config --global core.safecrlf true
 
 
